44bf59548f3cffbfecdd7b965853f27a218a456d
vjpl <jinette.peralta@gmail.com>
2017-05-03 16:53:19 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -99,7 +99,12 @@ define('WP_DEBUG', false);
 /* Multisite */
 
 define( 'WP_ALLOW_MULTISITE', true );
-
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'wordpress-azuref0af.azurewebsites.net');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 /* That's all, stop editing! Happy blogging. */
 
 //Relative URLs for swapping across app service deployment slots 
